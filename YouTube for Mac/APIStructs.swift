6b418f5741fe304d0d825500200b2d8935ac9c58
atomtables <76572470+atomtables@users.noreply.github.com>
2024-01-16 17:54:04 -0500

    basically everything, started this project in 2022 as a part of learning SwiftUI, but is unable to go anywhere due to the instability of the InvidiousAPI.

---
+++ YouTube for Mac/APIStructs.swift
@@ -0,0 +1,8 @@
+//
+//  APIStructs.swift
+//  YouTube for Mac
+//
+//  Created by Adithiya Venkatakrishnan on 9/8/2023.
+//
+
+import Foundation
---
+++ YouTube for Mac/VideoMoreInfo.swift
@@ -0,0 +1,18 @@
+//
+//  VideoMoreInfo.swift
+//  YouTube for Mac
+//
+//  Created by Adithiya Venkatakrishnan on 10/8/2023.
+//
+
+import SwiftUI
+
+struct VideoMoreInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoMoreInfo()
+}
